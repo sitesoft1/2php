98cc3342aded98b8ddda8c8fd641f87c9ce618b1
sitesoft1 <sitesoft1@gmail.com>
2020-09-08 22:25:50 +0300

    Создали таблицу в БД

--- App/Db.php
+++ App/Db.php
@@ -6,8 +6,18 @@ namespace App;
 
 class Db
 {
+    protected $dbh;
+    
     function __construct()
     {
-        echo 'Hellow Db';
+        echo 'Hellow Db<br>';
+        $this->dbh = new \PDO('mysql:host=localhost;dbname=2-php', 'root', '');
+    }
+    
+    public function execute($sql)
+    {
+        $sth = $this->dbh->prepare($sql);
+        $rezult = $sth->execute();
+        return $rezult;
     }
 }
--- index.php
+++ index.php
@@ -2,3 +2,6 @@
 require_once __DIR__ . '/autoload.php';
 
 $db = new App\Db();
+$rezult = $db->execute('CREATE TABLE foo (id SERIAL)');
+
+var_dump($rezult);
